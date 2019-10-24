445efb4537aa4a12ad967d49135b8c7dc260b170
zhenyabodukhin <zhenyabodukhin@mail.ru>
2019-10-24 19:03:23 +0300

    jd01_01 step2 of task C completed

--- src/by/it/bodukhin/jd01_02/TaskC.java
+++ src/by/it/bodukhin/jd01_02/TaskC.java
@@ -10,20 +10,26 @@ public class TaskC {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int mas[][] = new int[n][n];
-        step1(n);
+        int massum[][]=step1(n);
+        System.out.println(step2(massum));
     }
 
      static int[][] step1(int n) {
         int mas[][] = new int[n][n];
-
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas.length; j++) {
-                int k = (int) Math.round((Math.random() * (2 * n) - n));
-                mas[i][j] = k;
+        boolean min;
+        boolean max;
+        do {
+            min = false;
+            max = false;
+            for (int i = 0; i < mas.length; i++) {
+                for (int j = 0; j < mas.length; j++) {
+                    int k = (int) Math.round((Math.random() * (2 * n) - n));
+                    mas[i][j] = k;
+                    if (k == -n) min = true;
+                    if (k == n) max = true;
+                }
             }
-        }
-
+        } while (!min || !max);
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas.length; j++) {
                 System.out.print(mas[i][j] + " ");
@@ -31,6 +37,27 @@ public class TaskC {
             System.out.println();
         }
         return mas;
+    }
+
+    static int step2(int[][] mas) {
+        int result = 0;
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                if (count == 1 && mas[i][j] < 0 && mas[i].length - 1 > 0) {
+                    sum = sum + mas[i][j];
+                }
+                if (mas[i][j] > 0) {
+                    count++;
+                }
+            }
+            result = result + sum;
+
+            sum = 0;
+            count = 0;;
+        }
+        return result;
     }
 }
 
