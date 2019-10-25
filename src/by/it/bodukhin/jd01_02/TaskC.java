1c744395ae7082e9d774e36184651ed8507117a3
zhenyabodukhin <zhenyabodukhin@mail.ru>
2019-10-25 17:12:51 +0300

    jd01_02 bring some changes in step2 of task C jd01_02 step3 of task C completed jd01_02 task C completed

--- src/by/it/bodukhin/jd01_02/TaskC.java
+++ src/by/it/bodukhin/jd01_02/TaskC.java
@@ -1,7 +1,7 @@
 package by.it.bodukhin.jd01_02;
 
 
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -10,14 +10,16 @@ public class TaskC {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int massum[][]=step1(n);
-        System.out.println(step2(massum));
+        int matrix[][]=step1(n);
+        System.out.println(step2(matrix));
+        System.out.println(Arrays.deepToString(step3(matrix)));
     }
 
      static int[][] step1(int n) {
         int mas[][] = new int[n][n];
         boolean min;
         boolean max;
+        System.out.println("Исходная матрица");
         do {
             min = false;
             max = false;
@@ -30,6 +32,7 @@ public class TaskC {
                 }
             }
         } while (!min || !max);
+
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas.length; j++) {
                 System.out.print(mas[i][j] + " ");
@@ -43,20 +46,76 @@ public class TaskC {
         int result = 0;
         int sum = 0;
         int count = 0;
+        System.out.println("Сумма по строкам");
         for (int i = 0; i < mas.length; i++) {
             for (int j = 0; j < mas[i].length; j++) {
-                if (count == 1 && mas[i][j] < 0 && mas[i].length - 1 > 0) {
-                    sum = sum + mas[i][j];
+                if (count == 1 && mas[i][j] < 0 ) {
+                    if(j!=mas[i].length-1) {
+                        sum = sum + mas[i][j];
+                    }
                 }
                 if (mas[i][j] > 0) {
                     count++;
                 }
             }
             result = result + sum;
-
+            System.out.println(sum);
             sum = 0;
             count = 0;;
         }
+        System.out.println("Общая сумма всех строк ");
+        return result;
+    }
+
+     static int[][] step3(int[][]mas) {
+        int rows = mas.length;
+        int cols = mas.length;
+        boolean[] delRow = new boolean[mas.length];
+        boolean[] delCol = new boolean[mas.length];
+        int max = mas[0][0];
+         System.out.println("Матрица без строк и столбцов с максимумом");
+
+         for (int i = 1; i < mas.length; i++) {
+             for (int j = 1; j < mas[i].length; j++) {
+                 if (mas[i][j] > max) {
+                     max = mas[i][j];
+                 }
+             }
+         }
+
+        for (int i = 0; i < mas.length; i++) {
+            for (int j = 0; j < mas[i].length; j++) {
+                int k = mas[i][j];
+                if(k==max){
+                    delRow[i]=true;
+                }
+            }
+        }
+
+         for (int i = 0; i < mas.length; i++) {
+             for (int j = 0; j < mas[i].length; j++) {
+                 int k = mas[i][j];
+                 if(k==max){
+                     delCol[j]=true;
+                 }
+             }
+         }
+
+         for(boolean e : delRow) if (e) rows--;
+         for(boolean e : delCol) if (e) cols--;
+         int[][] result = new int [rows][cols];
+         int ir = 0;
+         for (int i = 0; i < mas.length; i++) {
+             if(!delRow[i]) {
+                 int jr = 0;
+                 for (int j = 0; j < mas[i].length; j++) {
+                     if(!delCol[j]){
+                         result[ir][jr++]=mas[i][j];
+                     }
+                 }
+                 ir++;
+             }
+         }
         return result;
     }
 }
