f9ad137a1272593e186105abc7a59a4d37a162ed
Pavel <mbsoftmb@gmail.com>
2019-10-27 17:18:41 +0300

    lesson 4 final

---
+++ src/by/it/yaroshenko/jd01_05/Test.java
@@ -0,0 +1,12 @@
+package by.it.yaroshenko.jd01_05;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        //int[] array = new int[];
+    }
+}
