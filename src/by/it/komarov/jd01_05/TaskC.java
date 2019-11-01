aa0c3b4f4f8d99bb1120bbdd484c169cbf333594
AntonKomarov <anton.komaro116@gmail.com>
2019-11-01 22:12:48 +0300

    Task B, C

--- src/by/it/komarov/jd01_05/TaskC.java
+++ src/by/it/komarov/jd01_05/TaskC.java
@@ -2,38 +2,43 @@ package by.it.komarov.jd01_05;
 
 public class TaskC {
     public static void main(String[] args) {
-        step1();
-    }
-    private static void step1() {
-        double[] arrayA = new double[40];
-        double[] arrayB = new double[40];
-        int indexA = -1;
-        int indexB = -1;
+        double[] arrayA = new double[30];
 
+
+        setArrayA(arrayA);
+        getArray(arrayA);
+        getArray(setArrayB(arrayA));
+
+    }
+
+    private static void setArrayA(double[] arrayA){
+        int indexA = -1;
         for (double x = 5.33; x <= 9.0 ; x += 0.15) {
             indexA++;
-            arrayA[indexA] = Math.pow((x*x) + 4.5, 1.0/3.0);
-            System.out.printf("A[%3d]=%4.6f ", indexA, arrayA[indexA]);
-            if (indexA % 5 == 0)
-                System.out.println();
-            if (arrayA[indexA] > 3.5){
-                indexB++;
-                arrayB[indexB] = arrayA[indexA];
-                }
-            }
-        System.out.println();
+            arrayA[indexA] = Math.pow((x * x) + 4.5, 1.0 / 3.0);
+        }
+    }
 
+    private static double[] setArrayB(double[] arrayA){
+        double[] arraySecond = new double[30];
+        int indexArray = 0;
         for (int i = 0; i < arrayA.length; i++) {
             if (arrayA[i] > 3.5){
-                arrayB[i] = arrayA[i];
-            }
-            if (arrayB[i] == 0){
-                continue;
-            }else
-                System.out.printf("B[%3d]=%4.6f ", i, arrayB[i]);
-            if (i % 5 == 0){
-                System.out.println();
+                arraySecond[indexArray++] = arrayA[i];
             }
         }
+        return arraySecond;
+    }
+
+    private static void getArray(double[] array){
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                System.out.printf("Array[%2d]=%3.6f ", i, array[i]);
+                if (i % 5 == 0) {
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println();
     }
 }
