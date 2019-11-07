f59fa1ab9994f42835d12ef363b5427ba0abebb5
Dasha Sermiazhko <dasha.sermiazhko@gmail.com>
2019-11-08 01:28:31 +0300

    add new func test_jd01_01_05

--- src/by/it/sermyazhko/jd01_05/TaskC.java
+++ src/by/it/sermyazhko/jd01_05/TaskC.java
@@ -1,5 +1,7 @@
 package by.it.sermyazhko.jd01_05;
 
+import com.sun.xml.internal.fastinfoset.stax.factory.StAXOutputFactory;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -29,6 +31,7 @@ public class TaskC {
                 ++count;
             }
         }
+        System.out.println("count = " + count);
         double[] arr = new double[count];
         for (int j = 0; j < arr.length; j++) {
             for (int i = temp; i < mas.length; i++) {
@@ -42,22 +45,158 @@ public class TaskC {
         //sort B
         by.it.sermyazhko.jd01_04.TaskC.mergeSort(arr,0,arr.length-1);
         System.out.println();
-        System.out.println("Массив A[]");
+        System.out.println("Massiv A <index to rows>");
         printArrayPsev(mas,"A",5);
-        System.out.println();
-        System.out.println("Массив B[]");
-        printArrayPsev(arr,"B",5);
+        System.out.println("Massiv B <index to cols>");
+        printArrayPsevToRow(arr,"B",5);
     }
 
-    //Реализовать + вывод по напр строк
+
     public static void printArrayPsev(double[] arr, String name, int columnCount) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.printf("%-1s[%3d]=%-10.1f", name, i, arr[i]);
-            if((i+1) % columnCount == 0 || i == arr.length - 1){
-                System.out.println();
+        System.out.printf("%s", '\u2554');
+        for (int i = 0; i < columnCount; i++) {
+            System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s", '\u2550', '\u2550', '\u2550', '\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550');
+            if(i==columnCount-1){
+                System.out.printf("%s", '\u2557');
+            }
+            else{
+                System.out.printf("%s",'\u2566');
             }
         }
+        System.out.println("");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%s%2s[%2d]=%-6.1f", '\u2551', name, i, arr[i]);
+            int end = columnCount - arr.length%columnCount;
+            if(i == arr.length - 1 && end != columnCount){
+                for (int h = 0; h < end; h++) {
+                    System.out.printf("%-14s", '\u2551');
+                }
+            }
+            if((i+1) % columnCount == 0 || i == arr.length - 1){
+                System.out.printf("%s", '\u2551');//конец каждой строки
+                System.out.println();
+                if(i!=arr.length-1){
+                    System.out.printf("%s", '\u2560');//начало каждой между строки
+                }
+                for (int j = 0; j < columnCount; j++) {
+                    if (i == arr.length - 1) {
+                        break;
+                    }
+                    if(j == columnCount-1){
+                        System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s", '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550');
+                        System.out.printf("%s", '\u2563');
+                    }
+                    else {
+                        System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s", '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550');
+                        System.out.printf("%s", '\u256C');
+                    }
+                }
+                if(i!=arr.length-1){
+                    System.out.println();
+                }
+            }
+        }
+        System.out.printf("%s",'\u255A');
+        for (int i = 0; i < columnCount; i++) {
+            System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s",  '\u2550', '\u2550', '\u2550', '\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550');
+            if(i == columnCount-1){
+                System.out.printf("%s", '\u255D');
+            }
+            else{
+                System.out.printf("%s",'\u2569');
+            }
+        }
+        System.out.println();
     }
+
+/*
+    public static void printArrayPsevToRow(double[] arr, String name, int rowCount) {
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = i; j < arr.length; j+=rowCount) {
+                System.out.printf("%-1s[%2d]=%-10.1f ", name, j, arr[j]);
+            }
+            System.out.println();
+        }
+    }
+*/
+
+    public static void printArrayPsevToRow(double[] arr, String name, int rowCount) {
+        int columnCount;
+        if(arr.length%rowCount == 0){
+            columnCount = arr.length/rowCount;
+        }
+        else {
+            columnCount = arr.length/rowCount + 1;
+        }
+
+        System.out.printf("%s", '\u2554');
+        for (int i = 0; i < columnCount; i++) {
+            System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s", '\u2550', '\u2550','\u2550', '\u2550', '\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550');
+            if(i==columnCount-1){
+                System.out.printf("%s", '\u2557');
+            }
+            else{
+                System.out.printf("%s",'\u2566');
+            }
+
+        }
+        System.out.println("");
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = i; j < arr.length; j+=rowCount) {
+
+                System.out.printf("%s%2s[%2d]=%-5.1f ", '\u2551',name, j, arr[j]);
+
+                if(j==i+(rowCount-1)*columnCount)
+                {
+                    System.out.printf("%s", '\u2551');//конец каждой строки
+                }
+
+
+            }
+           /* if(i+(columnCount-1)*rowCount >= arr.length){// && j>=arr.length){// && end != columnCount){
+                //for (int h = 0; h < end; h++) {
+                System.out.printf("%-14s", '\u2551');
+
+                // }
+            }*/
+
+
+            if(i!=rowCount - 1){
+                System.out.printf("\n%s", '\u2560');//начало каждой между строки
+            }
+
+            //System.out.println();
+            for (int j = 0; j < columnCount; j++) {
+                if (i == rowCount-1) {
+                    break;
+                }
+                if(j == columnCount-1){
+                    System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s", '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550');
+                    System.out.printf("%s", '\u2563');
+                }
+                else {
+                    System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s",'\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550', '\u2550');
+                    System.out.printf("%s", '\u256C');
+                }
+            }
+
+
+            System.out.println();
+        }
+
+        System.out.printf("%s",'\u255A');
+        for (int i = 0; i < columnCount; i++) {
+            System.out.printf("%s%s%s%s%s%s%s%s%s%s%s%s%s",  '\u2550', '\u2550', '\u2550', '\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550','\u2550');
+            if(i == columnCount-1){
+                System.out.printf("%s", '\u255D');
+            }
+            else{
+                System.out.printf("%s",'\u2569');
+            }
+        }
+
+    }
+
 
     private static void testTaskC1(double x_start, double x_end) {
         double[] mas = new double[28];
