79964e15ae5be1f2f81b690b88e937ba5ce7c23a
Anasteizha-01 <LilyOfTheValley-74>
2019-11-08 22:11:39 +0300

    vers 02. Добавлен бинарный поиск для Task 04

--- src/by/it/toporova/jd01_04/TaskC.java
+++ src/by/it/toporova/jd01_04/TaskC.java
@@ -5,19 +5,6 @@ import java.util.Scanner;
 
 public class TaskC {
 
-    //С1. Повторите полностью в этом классе задание A2(скопируйте в TaskC готовый метод
-    // static void buildOneDimArray(String line)и вызовите его из main, но сортировку переделайте
-    // (т.е. замените) из пузырьковой в более быструю сортировку слияниемсO-сложностью не выше, чем O(n log n).
-    //Метод для сортировки должен находиться в классе TaskCи иметь сигнатуру:static void mergeSort(double[ ] array)
-    //Подсказки: удобно предусмотреть также наличие второй копии этого метода для возможности рекурсивного вызова,
-    // например, с такой сигнатурой
-    //private static void mergeSort(double[ ] array, intleft, intright)
-    //метод для слияния двух массивов разработайте самостоятельно, его сигнатура может быть, например, вот такой
-    //private static double[ ] merge(double[ ] part1, double[ ] part2)
-    //С2. Также при повторе задания A2выполните вычисление новых (после сортировки) индексов первого и
-    // последнего элемента исходного массива с помощью собственного алгоритма двоичного (бинарного) поиска,
-    // который должен находиться в классе TaskCи иметь сигнатуру:static int binarySearch(double[ ] array, double value)
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -27,16 +14,23 @@ public class TaskC {
     }
 
 
-    static double[] buildOneDimArray(String line) {
+    private static double[] buildOneDimArray(String line) {
         double[] array = InOut.getArray(line);
         double start = array[0];
-        double end = array[array.length - 1];//указываем массив и место в массиве для последнего элемента. Длина массива ==10, значит индекс последнего элемента ==9
+        double end = array[array.length - 1];
+        //указываем массив и место в массиве для последнего элемента. Длина массива ==10, значит индекс последнего элемента ==9
         InOut.printArray(array, "V", 5);
-        mergeSort(array);
-        System.out.println(Arrays.toString(array));
+        double[] arr = mergeSort(array);
+        //    System.out.println(Arrays.toString(array));
         InOut.printArray(array, "V", 4);
 
-        for (int i = 0; i < array.length; i++) {
+        int firstIndex = binarySearch(arr, start);
+        System.out.println("Index of first element=" + firstIndex);
+        int lastIndex = binarySearch(arr, end);
+        System.out.println("Index of last element=" + lastIndex);
+
+
+     /* for (int i = 0; i < array.length; i++) {
             if (array[i] == start) { //значение элемента с координатами i д.б равно заданному стартовому
                 System.out.println("Index of first element=" + i);
                 break;
@@ -47,7 +41,7 @@ public class TaskC {
                 System.out.println("Index of last element=" + i);
                 break;
             }
-        }
+        }*/
         return array;
     }
 
@@ -105,18 +99,43 @@ public class TaskC {
         // 4 - начальное положение целевого массива, 5 - кол-во элементов, которые будут скопированы
         System.arraycopy(first, iFirst, result, iMerged, first.length - iFirst);
         System.arraycopy(second, iSecond, result, iMerged, second.length - iSecond);
+
+        //System.out.println("вывод результата" + Arrays.toString(result));
+
     }
+
+    private static int binarySearch(double[] array, double value) {
+        //double first = array[0];
+        //double last = array[array.length - 1];
+        int high = array.length - 1;
+        int low = 0;
+        int mid = (high + 1) / 2;
+        int index = -1;
+
+        while (low <= high) {
+
+            if (array[mid] < value) {
+                low = mid + 1;
+            } else if (array[mid] == value) {
+                index = mid;
+                System.out.println("Found");
+                break;
+            } else {
+                high = mid - 1;
+            }
+            mid = (low + high) / 2;
+        }
+        if (low > high) {
+            System.out.println("Fail");
+        }
+        return index;
+    }
+
 
 }
 
 
-//бинарный поиск первого и последнего
 
-
-// static int binarySearch(double[ ] array, double value){
-
-
-// }
 
 
 
