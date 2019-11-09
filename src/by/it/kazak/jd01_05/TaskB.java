2b3f0b6a8f9175650a3066b4fbd62bddf69eaab7
kzk <superman.vk97@gmail.com>
2019-11-09 18:03:28 +0300

    +tasks

--- src/by/it/kazak/jd01_05/TaskB.java
+++ src/by/it/kazak/jd01_05/TaskB.java
@@ -20,6 +20,28 @@ public class TaskB {
     }
 
     private static void step2() {
+        double x, a, b;
+        for (x = -2.75; x < 1; x += 0.25) {
+            if (x > -2 && x <= -1) {
+                b = Math.sin(4 * x * x);
+                a = Math.log10(Math.abs(b + 2.74));
+                System.out.printf("При x/2=%-6.2fa =%f%n", x, a);
+                continue;
+            }
+            if (x > -1 && x < 0.2) {
+                b = Math.cos(4 * x * x);
+                a = Math.log10(Math.abs(b + 2.74));
+                System.out.printf("При x/2=%-6.2fa =%f%n", x, a);
+                continue;
+            }
+            if ((2 * x) == 0.2) {
+                b = 1.0 / (Math.tan(4 * x * x));
+                a = Math.log10(Math.abs(b + 2.74));
+                System.out.printf("При x/2=%-6.2fa =%f%n", x, a);
+            } else {
+                System.out.println("При x/2=" + x + " " + "вычисления не определены");
+            }
 
+        }
     }
 }
