6e2e8d55f53a709010e0dbf2dc46e65844403258
Andrew <poppy.is.op@gmail.com>
2019-11-11 00:22:58 +0300

    java basic course jd01_05

---
+++ src/by/it/kharitonenko/jd01_06/TaskB1.java
@@ -0,0 +1,34 @@
+package by.it.kharitonenko.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+    private static final char[] vowels = {'а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я', 'А', 'О', 'И', 'Е', 'Ё', 'Э', 'Ы', 'У', 'Ю', 'Я'};
+
+    //Поиск подходящих слов
+    private static boolean checkChars(String word) {
+        char[] checkWord = word.toCharArray();
+        for (char vowel : vowels) {
+            if (vowel==checkWord[0]) {
+                return false;
+            }
+        }
+        for (char vowel : vowels) {
+            if (vowel==checkWord[checkWord.length-1])
+                return true;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            if (checkChars(matcher.group())) {
+                System.out.println(matcher.group());
+            }
+        }
+    }
+}
