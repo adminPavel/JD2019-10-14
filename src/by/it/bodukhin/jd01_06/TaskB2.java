7f9effc11845abfc5e204608146a561819e4df69
zhenyabodukhin <zhenyabodukhin@mail.ru>
2019-11-10 21:57:49 +0300

    jd01_06 TaskB2 format changes

--- src/by/it/bodukhin/jd01_06/TaskB2.java
+++ src/by/it/bodukhin/jd01_06/TaskB2.java
@@ -4,7 +4,7 @@ package by.it.bodukhin.jd01_06;
 public class TaskB2 {
 
     public static void main(String[] args) {
-        String delLong = Poem.text.replace("...","");
+        String delLong = Poem.text.replace("...","   ");
         String[] sentences = delLong.split("[.!]+");
         replace(sentences);
         compareSentences(sentences);
@@ -18,9 +18,9 @@ public class TaskB2 {
         for (int i = 0; i <sentences.length ; i++) {
             sentences[i] = sentences[i].replace(",\n", " ");
             sentences[i] = sentences[i].replace("\n", " ");
-            sentences[i] = sentences[i].replace(", ", " ");
-            sentences[i] = sentences[i].replace(" - "," ");
-            sentences[i] = sentences[i].replace(" : ", " ");
+            sentences[i] = sentences[i].replace(",", " ");
+            sentences[i] = sentences[i].replace("-"," ");
+            sentences[i] = sentences[i].replace(":", " ");
             sentences[i] = sentences[i].trim();
         }
     }
