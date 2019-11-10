094ee7d0930d6f0f5c1e8d8d56d1787cc2504478
zhenyabodukhin <zhenyabodukhin@mail.ru>
2019-11-10 17:12:31 +0300

    jd01_06 TaskC2 completed

---
+++ src/by/it/bodukhin/jd01_06/TaskC2.java
@@ -0,0 +1,51 @@
+package by.it.bodukhin.jd01_06;
+
+public class TaskC2 {
+    public static void main(String[] args) {
+        String Ptext = Poem.text;
+        String text = randomGenerateText(Ptext);
+
+        System.out.println(slow(text));
+        System.out.println(System.nanoTime());
+
+        System.out.println(fast(text));
+        System.out.println(System.nanoTime());
+    }
+
+    private static String slow(String text) {
+        String[] words = text.split("[ ]+");
+        String slowString = "";
+        do {
+            for (String word : words) {
+                slowString = slowString.concat(word).concat(" ");
+            }
+        } while (slowString.length()<100000);
+
+        return slowString;
+    }
+
+    private static String fast(String text) {
+        String[] words = text.split("[ ]+");
+        StringBuilder fastString = new StringBuilder();
+        do {
+            for (String word : words) {
+                fastString.append(word).append(" ");
+            }
+        } while (fastString.length()<100000);
+
+        return String.valueOf(fastString);
+    }
+
+    private static String randomGenerateText(String Ptext) {
+        String[] words = Ptext.split("[ ,-.:!\\n]+");
+        String randomText = "";
+        do {
+            for (int i = 0; i < 147; i++) {
+                int rword = (int) (Math.random() * 146);
+                randomText = randomText.concat(words[rword]).concat(" ");
+            }
+        } while (randomText.length()<100000);
+
+        return randomText;
+    }
+}
