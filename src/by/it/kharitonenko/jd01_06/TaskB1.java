04859e52f7cbb03f1de20927b10a7aede41dad23
Andrew <poppy.is.op@gmail.com>
2019-11-11 00:26:29 +0300

    java basic course jd01_06

--- src/by/it/kharitonenko/jd01_06/TaskB1.java
+++ src/by/it/kharitonenko/jd01_06/TaskB1.java
@@ -4,9 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB1 {
+    //все гласные символы
     private static final char[] vowels = {'а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я', 'А', 'О', 'И', 'Е', 'Ё', 'Э', 'Ы', 'У', 'Ю', 'Я'};
 
-    //Поиск подходящих слов
+    //поиск подходящих слов, сначала проверяется первая буква, затем последняя
     private static boolean checkChars(String word) {
         char[] checkWord = word.toCharArray();
         for (char vowel : vowels) {
