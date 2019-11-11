a7fbdafad23f55e538eecf6bd3035335f6a65c65
yurtsevichK <nicecurls@icloud.com>
2019-11-11 23:50:55 +0300

    Commit  jd07(a1)

--- src/by/it/yurtsevich/jd01_06/TaskB1.java
+++ src/by/it/yurtsevich/jd01_06/TaskB1.java
@@ -1,4 +1,19 @@
 package by.it.yurtsevich.jd01_06;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskB1 {
+    private static String[] words = new String[0];
+    private  static char[] vowels = {'а','о','и','е','ё','э','ы','у','ю','я'};
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[а-яА-яёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()){
+
+
+        }
+    }
 }
