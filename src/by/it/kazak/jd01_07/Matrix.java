2410726034009bc9c12664869e5828b3246dde5e
kzk <superman.vk97@gmail.com>
2019-11-11 18:09:47 +0300

    start C in jd01_07

--- src/by/it/kazak/jd01_07/Matrix.java
+++ src/by/it/kazak/jd01_07/Matrix.java
@@ -1,4 +1,28 @@
 package by.it.kazak.jd01_07;
 
-public class Matrix {
+public class Matrix extends Var{
+
+    private double[][] value;
+
+    Matrix(double[][] value) {
+        this.value = value;
+    }
+    Matrix(Matrix matrix) {
+        this.value = matrix.value;
+    }
+    public Matrix(String strMatrix){
+
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        String d = "";
+        for (double[] element : value) {
+            sb.append(d).append(element);
+            d = ", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
 }
+
