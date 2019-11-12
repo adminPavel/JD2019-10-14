c267864b0f0baf4a9b00df32033c05d054bd28e1
Andrew <poppy.is.op@gmail.com>
2019-11-12 22:05:58 +0300

    java basic course jd01_07 start

---
+++ src/by/it/kharitonenko/jd01_07/Scalar.java
@@ -0,0 +1,15 @@
+package by.it.kharitonenko.jd01_07;
+
+public class Scalar extends Var {
+    private double value;
+
+    Scalar(double value) {
+        this.value = value;
+    }
+    Scalar(Scalar scalar) {
+
+    }
+    Scalar(String strScalar) {
+
+    }
+}
