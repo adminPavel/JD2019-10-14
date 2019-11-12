a484dcff23ea35098830d5f046fecce0796f1f40
Andrew <poppy.is.op@gmail.com>
2019-11-13 02:55:22 +0300

    java basic course jd01_07 all done

--- src/by/it/kharitonenko/jd01_07/Scalar.java
+++ src/by/it/kharitonenko/jd01_07/Scalar.java
@@ -1,15 +1,20 @@
 package by.it.kharitonenko.jd01_07;
 
-public class Scalar extends Var {
+class Scalar extends Var {
     private double value;
 
     Scalar(double value) {
         this.value = value;
     }
-    Scalar(Scalar scalar) {
-
+    Scalar(Scalar otherScalar) {
+        this.value = otherScalar.value;
     }
     Scalar(String strScalar) {
+        this.value = Double.parseDouble(strScalar);
+    }
 
+    @Override
+    public String toString() {
+        return Double.toString(this.value);
     }
 }
--- src/by/it/kharitonenko/jd01_07/Var.java
+++ src/by/it/kharitonenko/jd01_07/Var.java
@@ -1,5 +1,8 @@
 package by.it.kharitonenko.jd01_07;
 
 abstract class Var {
-
+    @Override
+    public String toString() {
+        return "abstract Var";
+    }
 }
