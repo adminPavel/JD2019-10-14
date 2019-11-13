e7d45c1abb7a7f993c08f03bc3db89c6f38f63fa
AntonKomarov <anton.komaro116@gmail.com>
2019-11-13 23:36:13 +0300

    Test 8

--- src/by/it/komarov/jd01_08/Scalar.java
+++ src/by/it/komarov/jd01_08/Scalar.java
@@ -6,10 +6,6 @@ class Scalar extends Var{
 
     Scalar(double value)   { this.value = value; }
 
-    Scalar(String str)     { this.value = Double.parseDouble(str); }
-
-    Scalar(Scalar scalar)  { this.value = scalar.value; }
-
     @Override
     public String toString() { return Double.toString(value); }
 
