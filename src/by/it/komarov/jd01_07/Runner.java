cdf3cdbf73bb47ed26148aec40e9c97a55e16603
AntonKomarov <anton.komaro116@gmail.com>
2019-11-13 08:00:25 +0300

    Test 7

--- src/by/it/komarov/jd01_07/Runner.java
+++ src/by/it/komarov/jd01_07/Runner.java
@@ -4,25 +4,24 @@ public class Runner {
     public static void main(String[] args) {
 
         double value = 3.1415;
+        String line = "12";
         Scalar scalar1 = new Scalar(value);
         Var scalar2 = new Scalar(scalar1);
-        Var scalar3 = new Scalar("12");
+        Var scalar3 = new Scalar(line);
+        System.out.println(scalar1);
+        System.out.println(scalar2);
+        System.out.println(scalar3);
 
         double[] array = {1, 2, 4};
-        String line = "1.0,2.0,4.0";
         Vector vector1 = new Vector(array);
         Var vector2 = new Vector(vector1);
-        Var vector3 = new Vector(line);
-
-        System.out.println(vector1.toString());
-        System.out.println(vector2.toString());
-        System.out.println(vector3.toString());
+        System.out.println(vector1);
+        System.out.println(vector2);
 
         double[][] matrix = {{1, 2}, {3, 4}};
         Matrix matrix1 = new Matrix(matrix);
         Var matrix2 = new Matrix(matrix1);
-
-
-
+        System.out.println(matrix1);
+        System.out.println(matrix2);
     }
 }
