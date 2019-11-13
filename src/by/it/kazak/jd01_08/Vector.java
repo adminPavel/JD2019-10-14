04384ad2afaa0f66da364f7dc751716e8a7d34cb
kzk <superman.vk97@gmail.com>
2019-11-13 16:05:16 +0300

    jd01_08 start

---
+++ src/by/it/kazak/jd01_08/Vector.java
@@ -0,0 +1,121 @@
+package by.it.kazak.jd01_08;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+
+   private double[] value;
+   Vector(double[] value) {this.value = Arrays.copyOf(value,value.length);}
+   Vector(Vector vector) {this.value = vector.value;}
+
+   private Vector(String strVector){
+       String[] array_str = strVector.replaceAll("[{}]", "").split("[^\\d.]+");
+       double[] array_double = new double[array_str.length];
+       for (int i = 0; i < array_str.length; i++) {
+           array_double[i]=Double.parseDouble(array_str[i]);
+       }
+       this.value=array_double;
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double scalarValue = ((Scalar) other).getValue();
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] += scalarValue;
+            }
+            return new Vector(result);
+        } else if (other instanceof Vector &&
+                value.length == ((Vector) other).value.length
+        ) {
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] += ((Vector) other).value[i];
+            }
+            return new Vector(result);
+        } else
+            return super.add(other);
+    }
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double scalarValue = ((Scalar) other).getValue();
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] -= scalarValue;
+            }
+            return new Vector(result);
+        } else if (other instanceof Vector &&
+                value.length == ((Vector) other).value.length
+        ) {
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] -= ((Vector) other).value[i];
+            }
+            return new Vector(result);
+        } else
+            return super.add(other);
+    }
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double scalarValue = ((Scalar) other).getValue();
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] *= scalarValue;
+            }
+            return new Vector(result);
+        } else if (other instanceof Vector &&
+                value.length == ((Vector) other).value.length
+        ) {
+            double res = 0;
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                res += result[i] * ((Vector) other).value[i];
+            }
+            return new Vector(String.valueOf(res));
+        } else
+            return super.add(other);
+    }
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar) {
+            double scalarValue = ((Scalar) other).getValue();
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] /= scalarValue;
+            }
+            return new Vector(result);
+        } else if (other instanceof Vector &&
+                value.length == ((Vector) other).value.length
+        ) {
+            double[] result = new double[value.length];
+            System.arraycopy(value, 0, result, 0, value.length);
+            for (int i = 0; i < result.length; i++) {
+                result[i] /= ((Vector) other).value[i];
+            }
+            return new Vector(result);
+        } else
+            return super.add(other);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        String d = "";
+        for (double element : value) {
+            sb.append(d).append(element);
+            d = ", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
