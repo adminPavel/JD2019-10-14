67efa547f375e43ec2aa6ba1189558ba420f6b94
Pavel <mbsoftmb@gmail.com>
2019-11-15 19:52:18 +0300

    lesson 8 AB 1

---
+++ src/by/it/yaroshenko/jd01_09/Var.java
@@ -0,0 +1,35 @@
+package by.it.yaroshenko.jd01_09;
+
+abstract class Var implements Operation{
+
+
+    @Override
+    public String toString() {
+        return "This is Var class";
+    }
+
+    @Override
+    public Var add(Var other) {
+        System.out.printf("Сложение %s + %s невозможно\n", other, this);
+        return null;
+    }
+
+    @Override
+    public Var sub(Var other) {
+        System.out.printf("Вычитание %s + %s невозможно\n", other, this);
+        return null;
+    }
+
+    @Override
+    public Var mul(Var other) {
+        System.out.printf("Умножение %s + %s невозможно\n", other, this);
+        return null;
+    }
+
+    @Override
+    public Var div(Var other) {
+        System.out.printf("Деление %s + %s невозможно\n", other, this);
+        return null;
+    }
+
+}
