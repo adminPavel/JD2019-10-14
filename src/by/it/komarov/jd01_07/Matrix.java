d87a4b599c06f771f8bdd1555c6aaf4f3922553d
AntonKomarov <anton.komaro116@gmail.com>
2019-11-15 22:36:52 +0300

    Test 8

--- src/by/it/komarov/jd01_07/Matrix.java
+++ src/by/it/komarov/jd01_07/Matrix.java
@@ -11,6 +11,18 @@ public class Matrix extends Var{
         this.matrix = matrix.matrix;
     }
 
+    Matrix(String strMatrix){
+        String [] array = strMatrix.split("(},)");
+        matrix = new double[array.length][];
+        for (int i = 0; i < matrix.length; i++) {
+            String[] temp = (array[i].replaceAll("[{}]","")).split("[,]");
+            matrix[i] = new double[temp.length];
+            for (int j = 0; j < temp.length; j++) {
+                matrix[i][j] = Double.parseDouble(temp[j]);
+            }
+        }
+    }
+
 
     @Override
     public String toString() {
--- src/by/it/komarov/jd01_08/Matrix.java
+++ src/by/it/komarov/jd01_08/Matrix.java
@@ -15,6 +15,26 @@ public class Matrix extends Var{
     }
 
     @Override
+    public Var add(Var other) {
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        return super.div(other);
+    }
+
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder("{");
         String delimiter = "";
