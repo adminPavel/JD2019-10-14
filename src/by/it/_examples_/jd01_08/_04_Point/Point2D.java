e8dc75c01d3246fa875ed1510fd56d35fe2c8a32
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-11-16 10:33:52 +0300

    Tasks 6-8

--- src/by/it/_examples_/jd01_08/_04_Point/Point2D.java
+++ src/by/it/_examples_/jd01_08/_04_Point/Point2D.java
@@ -2,10 +2,12 @@ package by.it._examples_.jd01_08._04_Point;
 
 public class Point2D extends Point1D {
     private int y;
+
     public Point2D(int x, int y) {
         super(x);
         this.y = y;
     }
+
     @Override
     public double length() {
         return Math.hypot(super.length(), y);
@@ -13,6 +15,7 @@ public class Point2D extends Point1D {
     вызывать сам себя, что приведет к бесконечной
     рекурсии и ошибке во время выполнения */
     }
+
     @Override
     public String toString() {
         return super.toString() + " y=" + y;
