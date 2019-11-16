1916f6243433964d31c1ea19d6320879a7b7d824
Anasteizha-01 <LilyOfTheValley-74>
2019-11-16 11:55:30 +0300

    c3 для Task08 завершен. весь Task08 закончен

--- src/by/it/toporova/jd01_08/Matrix.java
+++ src/by/it/toporova/jd01_08/Matrix.java
@@ -10,7 +10,7 @@ public class Matrix extends Var {
         this.value = otherMatrix.value;
     }
 
-    Matrix(double[][] value) {
+    private Matrix(double[][] value) {
         this.value = Arrays.copyOf(Arrays.copyOf(value, value[0].length), value.length);//копірованіе строк на глубіну value[0].length
     }
 
@@ -92,11 +92,31 @@ public class Matrix extends Var {
             return new Matrix(result);
         }
 
-        if (other instanceof Vector){
-
+        if (other instanceof Vector
+                && value[0].length==((Vector) other).getValue().length){
+            double[] result = new double[value.length];
+            for (int i = 0; i < value.length; i++) {
+                for (int j = 0; j < value[i].length; j++) {
+                    result[i] = (value[i][j] * ((Vector) other).getValue()[j]) + result[i];
+                }
+            }
+            return new Vector(result);
         }
-        return null;
+        if (other instanceof Matrix
+                && value[0].length==((Matrix) other).value.length){
+            double[][] result = new double[value.length][((Matrix) other).value[0].length];
+            for (int i = 0; i < value.length; i++) {
+                for (int j = 0; j < value[0].length; j++) {
+                    for (int k = 0; k < ((Matrix) other).value[0].length; k++) {
+                        result[i][k] = (value[i][j] * ((Matrix) other).value[j][k]) + result[i][k];
+                    }
+                }
+            }
+            return new Matrix(result);
+        }
+        return super.mul(other);
     }
+
 
     
     
