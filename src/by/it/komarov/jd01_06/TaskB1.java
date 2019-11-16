1017588fd1005ff19da373f60ee9e73d055968b4
AntonKomarov <anton.komaro116@gmail.com>
2019-11-16 23:14:44 +0300

    Test 6

--- src/by/it/komarov/jd01_06/TaskB1.java
+++ src/by/it/komarov/jd01_06/TaskB1.java
@@ -1,20 +1,25 @@
 package by.it.komarov.jd01_06;
 
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskB1 {
+    private static final String vowels = "уеэоаыяиюёУЕЭОАЫЯИЮЁ";
+
     public static void main(String[] args) {
+        
         StringBuilder sb = new StringBuilder(Poem.text);
-        Pattern pattern = Pattern.compile("[цкнгшщзхфвпрлджчсмтбЦКНГШЩЗХФВПРЛДЖЧСМТБ]-[ёйуеъыаоэяиью]]+[$ёйуеъыаоэяиью]");
+        Pattern pattern = Pattern.compile("\\b[цкнгшщзхфвпрлджчсмтбЦКНГШЩЗХФВПРЛДЖЧСМТБ]+[а-яА-ЯёЁ]*[йуеъыаоэяиюьё]+\\b");
         Matcher matcher = pattern.matcher(Poem.text);
 
-        while (matcher.find()) {
-            String word = matcher.group();
-            System.out.println(matcher.group());
-
+        while (matcher.find()){
+            String words = matcher.group();
+            if (checkWord(words))
+            System.out.println(words);
         }
+    }
 
+    private static boolean checkWord(String word) {
+        return (vowels.indexOf(word.charAt(0)) < 0) && (vowels.indexOf(word.charAt(word.length() - 1)) >= 0);
     }
 }
