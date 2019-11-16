c323d08ddac825838011e1f2ab7007b07399f22c
AntonKomarov <anton.komaro116@gmail.com>
2019-11-16 16:49:55 +0300

    Test 6

---
+++ src/by/it/komarov/jd01_06/TaskB1.java
@@ -0,0 +1,20 @@
+package by.it.komarov.jd01_06;
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(Poem.text);
+        Pattern pattern = Pattern.compile("[цкнгшщзхфвпрлджчсмтбЦКНГШЩЗХФВПРЛДЖЧСМТБ]-[ёйуеъыаоэяиью]]+[$ёйуеъыаоэяиью]");
+        Matcher matcher = pattern.matcher(Poem.text);
+
+        while (matcher.find()) {
+            String word = matcher.group();
+            System.out.println(matcher.group());
+
+        }
+
+    }
+}
