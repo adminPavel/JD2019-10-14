2672510b98bd6747b0b4d07b26f8fae4826b16b9
yegor-volchek <yegor_volchek@mail.ru>
2019-11-17 14:41:57 +0300

    01_07 ABC 01_08 A

---
+++ src/by/it/volchek/jd01_08/Runner.java
@@ -0,0 +1,11 @@
+package by.it.volchek.jd01_08;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var scalar = new Scalar(new Scalar(2.3));
+        Var scalar2 = new Scalar("3.2");
+        System.out.println(scalar.toString() + " " + scalar2.toString());
+        Var vector = new Vector("{1.0, 3.5, 6.1");
+        System.out.println(vector.toString());
+    }
+}
