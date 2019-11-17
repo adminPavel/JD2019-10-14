64ea602a0c54ccf6ed9b6df09183692719fd631f
Andrew <poppy.is.op@gmail.com>
2019-11-17 20:44:12 +0300

    java basic course jd01_09 all done

---
+++ src/by/it/kharitonenko/jd01_09/Patterns.java
@@ -0,0 +1,8 @@
+package by.it.kharitonenko.jd01_09;
+
+public class Patterns {
+    static final String OPERATION = "[-+/*]";
+    static final String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    static final String VECTOR = "\\{"+SCALAR+"(,"+SCALAR+")*}";
+    static final String MATRIX = "\\{"+VECTOR+"(,"+VECTOR+")*}";
+}
