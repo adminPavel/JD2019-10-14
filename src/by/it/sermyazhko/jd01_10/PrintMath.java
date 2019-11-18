149ca03d0492162d26c3ed4d81c26fa4c80b4dc0
Alex Snetko <007alex03@gmail.com>
2019-11-18 12:02:03 +0300

    add B1 jd10

---
+++ src/by/it/sermyazhko/jd01_10/PrintMath.java
@@ -0,0 +1,24 @@
+package by.it.sermyazhko.jd01_10;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class PrintMath {
+
+    public static void main(String[] args) {
+        Class<Math> structure = Math.class;
+       //structure.getDeclaredMethod()
+        for (Method method: structure.getDeclaredMethods()){
+            int modifier = method.getModifiers();
+            /*if ((modifier&1)==1){
+                System.out.println(method);
+            }*/
+            if(Modifier.isPublic(modifier)){
+                StringBuilder sb = new StringBuilder("public ");
+                sb.append(method.getName()).append("(");
+                System.out.println(sb);
+            }
+        }
+
+    }
+}
