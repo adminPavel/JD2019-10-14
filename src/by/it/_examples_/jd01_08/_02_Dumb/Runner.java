680a60cc3da6eeedfd143c85f421a66c4c17abd4
MatiusP <45049910+MatiusP@users.noreply.github.com>
2019-11-18 12:11:37 +0300

    Tasks 6-8

--- src/by/it/_examples_/jd01_08/_02_Dumb/Runner.java
+++ src/by/it/_examples_/jd01_08/_02_Dumb/Runner.java
@@ -1,8 +1,7 @@
 package by.it._examples_.jd01_08._02_Dumb;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[ ] args) {
         Dumb objA = new Dumber();
-
     }
 }
