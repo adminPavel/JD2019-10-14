3d7c0f01deb39f57efd9c47f0fdd7bb979e05978
kzk <superman.vk97@gmail.com>
2019-11-19 19:45:35 +0300

    add some tasks in jd01_10

--- src/by/it/kazak/jd01_10/PrintMath.java
+++ src/by/it/kazak/jd01_10/PrintMath.java
@@ -1,5 +1,6 @@
 package by.it.kazak.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -9,20 +10,22 @@ public class PrintMath {
         Class<Math> strMath = Math.class;
         Method[] methods = strMath.getDeclaredMethods();
         for (Method method : methods) {
-            if ((method.getModifiers() & Modifier.PUBLIC) ==Modifier.PUBLIC) {
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
                 StringBuilder sb = new StringBuilder("public ");
                 sb.append("static ").append(method.getReturnType()).append(" ").append(method.getName()).append("(")
-                        .append(Arrays.toString(method.getParameterTypes())
-                            .replace("[","")
-                                .replace("]","")
-                                .replace(" ",""))
-                        .append(")");
-
+                        .append(Arrays.toString(method.getParameterTypes()).replace("[", "")
+                                .replace("]", "").replace(" ", "")).append(")");
                 System.out.println(sb);
             }
         }
-        //i can't understand how to make this part
-        System.out.println("double E");
-        System.out.println("double PI");
+        Field[] fields = Math.class.getFields();
+        for (Field field : fields) {
+            int mod = field.getModifiers();
+            if (Modifier.isPublic(mod)) {
+                if (Modifier.isStatic(mod)) {
+                    System.out.println(String.valueOf(field.getType()) + ' ' + field.getName());
+                }
+            }
+        }
     }
 }
--- src/by/it/kazak/jd01_10/PrintString.java
+++ src/by/it/kazak/jd01_10/PrintString.java
@@ -6,15 +6,13 @@ import java.lang.reflect.Modifier;
 
 public class PrintString {
     public static void main(String[] args) {
-
         Class<String> stringClass = String.class;
         Method[] strings = stringClass.getDeclaredMethods();
         for (Method method : strings) {
-            if ((method.getModifiers() & Modifier.STATIC) !=Modifier.STATIC);
-
-
-
-            System.out.println(method.getName());
+            if (!Modifier.isStatic(method.getModifiers())) {
+                System.out.println(method.getName());
+            }
         }
     }
 }
+
