cd1f52ce4ce1df85b9c475e88d890ffa76c57c51
Andrew <poppy.is.op@gmail.com>
2019-11-20 23:03:26 +0300

    java basic course jd01_11 start

---
+++ src/by/it/kharitonenko/jd01_11/ListA.java
@@ -0,0 +1,147 @@
+package by.it.kharitonenko.jd01_11;
+
+import java.util.*;
+
+class ListA<T> implements List<T>{
+
+    private T[] elements = (T[]) new Object[1];
+    private int size = 0;
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "List is empty.";
+        }
+        StringBuilder strBuilder = new StringBuilder();
+        String decimator = "";
+        strBuilder.append("[");
+        for (int i = 0; i < size; i++) {
+            strBuilder.append(elements[i]).append(decimator);
+            decimator = ", ";
+        }
+        strBuilder.append("]");
+        return strBuilder.toString();
+    }
+
+    @Override
+    public boolean add(T element) {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, elements.length*3/2+1);
+        }
+        elements[size++] = element;
+        return true;
+    }
+
+    //TODO
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return elements[index];
+    }
+
+    /* =========================DON'T NEED========================== */
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+}
