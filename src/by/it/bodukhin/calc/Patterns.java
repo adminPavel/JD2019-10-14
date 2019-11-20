956e3e7520cfc17a4ea5c8a847c798e2aeef7c51
zhenyabodukhin <zhenyabodukhin@mail.ru>
2019-11-20 11:53:39 +0300

    jd01_11 started

---
+++ src/by/it/bodukhin/calc/Patterns.java
@@ -0,0 +1,8 @@
+package by.it.bodukhin.calc;
+
+interface Patterns {
+    String SCALAR="-?[0-9]+(\\.[0-9]+)?";
+    String VECTOR="\\{"+SCALAR+"(,"+SCALAR+")*}";
+    String MATRIX="\\{"+VECTOR+"(,"+VECTOR+")*}";
+    String OPERATION="[-+*/]";
+}
