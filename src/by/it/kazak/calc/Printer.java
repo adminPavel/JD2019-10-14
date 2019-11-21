a610cb619f88fa783d1f22151d8a26cf509d575e
kzk <superman.vk97@gmail.com>
2019-11-21 16:15:02 +0300

    add calculator

---
+++ src/by/it/kazak/calc/Printer.java
@@ -0,0 +1,13 @@
+package by.it.kazak.calc;
+
+import java.util.Map;
+
+class Printer {
+
+    public static void printVar(Map<String, Var> sortArray) {
+    }
+
+    void print(Var result) {
+        System.out.println(result);
+    }
+}
