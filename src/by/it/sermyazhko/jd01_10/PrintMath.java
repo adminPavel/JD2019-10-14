db6b4c5bfc4e4a032eccf6eea75a46748b8c1de4
dachuleshka <dachuleshka@mail.ru>
2019-11-21 16:31:02 +0300

    add commit

--- src/by/it/sermyazhko/jd01_10/PrintMath.java
+++ src/by/it/sermyazhko/jd01_10/PrintMath.java
@@ -34,7 +34,6 @@ class PrintMath {
                         } else {
                             sb.append(",");
                         }
-
                     }
                 }
             }
---
+++ src/by/it/sermyazhko/jd01_11/ListA.java
@@ -0,0 +1,8 @@
+package by.it.sermyazhko.jd01_11;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class ListA<T> extends HashMap {
+
+}
