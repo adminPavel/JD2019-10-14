949f86ae9d02fc47791f742b3031c962d336f1ea
Andrew <poppy.is.op@gmail.com>
2019-11-24 23:18:17 +0300

    java basic course jd01_12 AB done

--- src/by/it/kharitonenko/jd01_12/TaskB3.java
+++ src/by/it/kharitonenko/jd01_12/TaskB3.java
@@ -1,9 +1,6 @@
 package by.it.kharitonenko.jd01_12;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.*;
 
 
 class TaskB3 {
@@ -29,24 +26,12 @@ class TaskB3 {
     }
 
     static String process(LinkedList<String> peoples) {
-        ListIterator<String> iterator = peoples.listIterator();
-        int counter = 0;
-        while (peoples.size() != 1) {
-            if (!iterator.hasNext()) {
-                iterator = peoples.listIterator();
-            }
-            iterator.next();
-            counter++;
-            if (!iterator.hasNext()) {
-                iterator = peoples.listIterator();
-            }
-            if (counter == 1) {
-                iterator.next();
-                iterator.remove();
-                counter = 0;
-            }
+        Deque<String> peopleDeque = peoples;
+        while (peopleDeque.size() != 1) {
+            peopleDeque.addLast(peopleDeque.remove());
+            peopleDeque.remove();
         }
-        return "Остался только " + peoples.get(0);
+        return "Остался только " + peopleDeque.peek();
     }
 
     private static String[] createArrayByNumber(int numberOfPeople) {
