7c94904f61ece0f415ff45409648743aaa094627
Andrew <poppy.is.op@gmail.com>
2019-11-28 15:42:51 +0300

    java basic course jd01_12 AB done

--- src/by/it/kharitonenko/calc/Scalar.java
+++ src/by/it/kharitonenko/calc/Scalar.java
@@ -6,9 +6,11 @@ class Scalar extends Var {
     Scalar(double value) {
         this.value = value;
     }
+
     Scalar(Scalar otherScalar) {
         this.value = otherScalar.value;
     }
+
     Scalar(String strScalar) {
         this.value = Double.parseDouble(strScalar);
     }
@@ -19,43 +21,35 @@ class Scalar extends Var {
     }
 
     @Override
-    public Var add(Var other) {
-        if (other instanceof Scalar)
-        {
+    public Var add(Var other) throws CalcException {
+        if (other instanceof Scalar) {
             return new Scalar(this.value + ((Scalar) other).value);
-        }
-         else
+        } else
             return other.add(this);
     }
 
     @Override
-    public Var sub(Var other) {
-        if (other instanceof Scalar)
-        {
+    public Var sub(Var other) throws CalcException {
+        if (other instanceof Scalar) {
             return new Scalar(this.value - ((Scalar) other).value);
-        }
-        else
+        } else
             return new Scalar(-1).mul(other).add(this);
     }
 
     @Override
-    public Var mul(Var other) {
-        if (other instanceof Scalar)
-        {
+    public Var mul(Var other) throws CalcException {
+        if (other instanceof Scalar) {
             return new Scalar(this.value * ((Scalar) other).value);
-        }
-        else
+        } else
             return other.mul(this);
     }
 
     @Override
-    public Var div(Var other) {
-        if (other instanceof Scalar)
-        {
-            if (((Scalar) other).value == 0) return super.div(other); //check for division by zero
+    public Var div(Var other) throws CalcException {
+        if (other instanceof Scalar) {
+            if (((Scalar) other).value == 0) throw new CalcException("divided by zero"); //check for division by zero
             return new Scalar(this.value / ((Scalar) other).value);
-        }
-        else
+        } else
             return other.add(this);
     }
 }
--- src/by/it/kharitonenko/calc/Var.java
+++ src/by/it/kharitonenko/calc/Var.java
@@ -7,29 +7,31 @@ abstract class Var implements Operation {
     }
 
     @Override
-    public Var add(Var other) {
-        System.out.printf("Сложение %s + %s невозможно\n",this,other);
+    public Var add(Var other) throws CalcException()
+
+    {
+        System.out.printf("Сложение %s + %s невозможно\n", this, other);
         //TODO add exception
         return null;
     }
 
     @Override
-    public Var sub(Var other) {
-        System.out.printf("Вычитание %s - %s невозможно\n",this,other);
+    public Var sub(Var other) throws CalcException {
+        System.out.printf("Вычитание %s - %s невозможно\n", this, other);
         //TODO add exception
         return null;
     }
 
     @Override
-    public Var mul(Var other) {
-        System.out.printf("Умножение %s * %s невозможно\n",this,other);
+    public Var mul(Var other) throws CalcException {
+        System.out.printf("Умножение %s * %s невозможно\n", this, other);
         //TODO add exception
         return null;
     }
 
     @Override
-    public Var div(Var other) {
-        System.out.printf("Деление %s / %s невозможно\n",this,other);
+    public Var div(Var other) throws CalcException {
+        System.out.printf("Деление %s / %s невозможно\n", this, other);
         //TODO add exception
         return null;
     }
