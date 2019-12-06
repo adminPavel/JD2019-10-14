34dac94d874ac915a83e747989a4c1028c1134f2
loktev <loktev@tut.by>
2019-12-06 12:09:01 +0300

    lesson02_03 level A

--- src/by/it/akhmelev/jd02_03/Buyer.java
+++ src/by/it/akhmelev/jd02_03/Buyer.java
@@ -2,9 +2,14 @@ package by.it.akhmelev.jd02_03;
 
 class Buyer extends Thread implements IBuyer {
 
+    private boolean waitFlag = false;
+
+    public void setWaitFlag(boolean waitFlag) {
+        this.waitFlag = waitFlag;
+    }
 
     Buyer(int number) {
-        super("Buyer №"+number);
+        super("Buyer №" + number);
         Dispatcher.buyerInMarket();
     }
 
@@ -19,32 +24,35 @@ class Buyer extends Thread implements IBuyer {
 
     @Override
     public void enterToMarket() {
-        System.out.println(this+" come to market");
+        System.out.println(this + " come to market");
     }
 
     @Override
     public void chooseGoods() {
-        System.out.println(this+" started to choose goods");
+        System.out.println(this + " started to choose goods");
         int timeout = Helper.random(500, 2000);
         Helper.sleep(timeout);
-        System.out.println(this+" finished to choose goods");
+        System.out.println(this + " finished to choose goods");
     }
 
     @Override
     public void goToQueue() {
         QueueBuyer.add(this);
-        synchronized (this){
+        waitFlag = true;
+        synchronized (this) {
             try {
-                this.wait();
+                while (waitFlag)
+                    this.wait();
             } catch (InterruptedException e) {
-                System.out.println("What???");
+                throw new RuntimeException(e);
             }
         }
     }
 
+
     @Override
     public void goOut() {
-        System.out.println(this+" leave the market");
+        System.out.println(this + " leave the market");
     }
 
     @Override
