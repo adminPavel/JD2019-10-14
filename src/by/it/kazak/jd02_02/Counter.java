a2ce144046940bfddd08b32b89ca7b5d01344987
kzk <superman.vk97@gmail.com>
2019-12-06 16:40:42 +0300

    jd02_02

---
+++ src/by/it/kazak/jd02_02/Counter.java
@@ -0,0 +1,27 @@
+package by.it.kazak.jd02_02;
+
+import java.util.HashMap;
+
+class Counter {
+
+    static int countBuyer = 0;
+    static int kSpeed = 10000;
+    final static int finishTime = 60;
+    private static HashMap<String, Double> products = new HashMap<String, Double>() {
+        {
+            put("potato", 2.0);
+            put("milk", 4.1);
+            put("bread", 1.5);
+            put("eggs", 3.2);
+            put("bubble gum", 1.2);
+            put("rice", 1.7);
+            put("peanuts", 4.3);
+            put("water", 0.5);
+        }
+    };
+
+    static HashMap<String, Double> getGoods() {
+        return products;
+    }
+
+}
