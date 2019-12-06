736b0cc84bdd246e66238cb6661dd6ff02f9d989
loktev <loktev@tut.by>
2019-12-06 11:27:46 +0300

    lesson02_03 level A

---
+++ src/by/it/akhmelev/jd02_03/QueueBuyer.java
@@ -0,0 +1,23 @@
+package by.it.akhmelev.jd02_03;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class QueueBuyer {
+
+    private final static Deque<Buyer> queue = new LinkedList<>();
+
+    static void add(Buyer buyer) {
+        synchronized (queue) {
+            queue.addLast(buyer);
+        }
+    }
+
+    static Buyer poll() {
+        synchronized (queue) {
+            return queue.pollFirst();
+        }
+    }
+
+
+}
