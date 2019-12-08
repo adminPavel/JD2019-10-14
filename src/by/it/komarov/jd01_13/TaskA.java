f82f0bc490ae1f07d420dd527cfe8ae8bed81219
AntonKomarov <anton.komaro116@gmail.com>
2019-12-08 14:48:48 +0300

    Tests

--- src/by/it/komarov/jd01_13/TaskA.java
+++ src/by/it/komarov/jd01_13/TaskA.java
@@ -5,25 +5,23 @@ import java.util.HashMap;
 public class TaskA {
     public static void main(String[] args) {
         try {
-
             if (Math.random() > 0.5)
                 new HashMap<String, String>(null);
             else
                 Integer.parseInt("привет");
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        } catch (NumberFormatException e) {
-            StackTraceElement[] stackTrace = e.getStackTrace();
-            for (StackTraceElement element : stackTrace) {
-                // проверка если элемент из stackTrace имеет такую же ошибку как и наш класс
-                // для этого воспользуемся Reflection API
-                // Reflection API - механизм исследования данных во время её выполнения(тёмная сторона Java)
-                if (TaskA.class.getName().equals(element.getClassName())) {
-                    System.out.println(element);
-                    break;
-                    // прерываем цикл чтобы не обрабатывались остальные элементы массива
+        } catch (NullPointerException | NumberFormatException e) {
+                StackTraceElement[] stackTrace = e.getStackTrace();
+                for (StackTraceElement element : stackTrace) {
+                    if (element.getClassName().equals(TaskA.class.getName())) {
+                        int lineNumber = element.getLineNumber();
+                        String className = element.getClassName();
+                        System.out.printf(" name: %s\nclass: %s\n line: %d\n",
+                                e.getClass().getName(),
+                                className,
+                                lineNumber);
+                        break;
+                    }
                 }
-            }
         }
     }
 }
