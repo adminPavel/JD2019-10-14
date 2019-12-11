3d98afeffdce920ae28d224805c5f5ef2d4af5a5
loktev <loktev@tut.by>
2019-12-11 12:30:42 +0300

    level A 02_05

---
+++ src/by/it/akhmelev/jd02_05/Messages.java
@@ -0,0 +1,7 @@
+package by.it.akhmelev.jd02_05;
+
+public interface Messages {
+    String WELCOME="message.welcome";
+    String QUESTION="message.question";
+
+}
---
+++ src/by/it/akhmelev/jd02_05/txt/convertor.bat
@@ -0,0 +1,3 @@
+native2ascii -encoding utf-8 translate.txt ../lang/translate.properties
+native2ascii -encoding utf-8 translate_ru_RU.txt ../lang/translate_ru_RU.properties
+native2ascii -encoding utf-8 translate_ru_RU.txt ../lang/translate_be_BY.properties
