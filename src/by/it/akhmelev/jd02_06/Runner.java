796e5341fb7416c8a22f5d1251ae195129d72bbd
loktev <loktev@tut.by>
2019-12-13 12:27:39 +0300

    level A 02_06

---
+++ src/by/it/akhmelev/jd02_06/Runner.java
@@ -0,0 +1,10 @@
+package by.it.akhmelev.jd02_06;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Logger.get().log("Hi!");
+        Logger.get().log("By!");
+    }
+
+}
