b162917997ed00a82d1b61b1e66528c17d0b232c
adminPavel <56544890+adminPavel@users.noreply.github.com>
2021-05-13 23:26:07 +0300

    Delete src/by/it/zimina directory

--- src/by/it/zimina/calc/Printer.java
+++
@@ -1,8 +0,0 @@
-package by.it.zimina.calc;
-
-public class Printer {
-    void print(Var var){
-        if (var!=null)
-            System.out.println(var);
-    }
-}
--- src/by/it/zimina/jd01_07/Var.java
+++
@@ -1,9 +0,0 @@
-package by.it.zimina.jd01_07;
-
-abstract class Var {
-
-    @Override
-    public String toString() {
-        return ("Это класс Var");
-    }
-}
