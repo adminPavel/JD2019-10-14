9538ea148b5621884994148e60892137d745784e
Pavel <mbsoftmb@gmail.com>
2023-12-06 15:05:21 +0300

    commit

--- src/by/it/yaroshenko/testing/ConstructorCopy.java
+++ src/by/it/yaroshenko/testing/ConstructorCopy.java
@@ -3,7 +3,12 @@ package by.it.yaroshenko.testing;
 public class ConstructorCopy {
         private String name;
         private int age;
-        
+    public ConstructorCopy(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
     public static void main(String[] args) {
 
     }
